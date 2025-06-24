c3cd141de65404148710d39dedf333647d74f778
sebastian lapt <smoreira11@hotmail.com>
2025-06-25 08:10:25 +0900

    add

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] uSers =  {"Administrator" "Manager" "User"}
+bool validEntry = false;
+Console.WriteLine("Enter your role name:");
+do
+{
+    uSers = Console.ReadLine();
+    if (uSers != null)
+    {
+        if (uSers.Length >= 1)
+        {
+            validEntry = true;
+        }
+        else
+        {
+            Console.WriteLine("Your input is invalid , please try again.");
+        }
+    }
+} while (validEntry == false);
